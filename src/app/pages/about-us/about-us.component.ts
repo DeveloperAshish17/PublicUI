4382f59af034713e8f02aecff2872e6991c7e80d
DeveloperAshish17 <yadavashish0798@gmail.com>
2022-09-18 16:40:21 +0530

    add some page

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -9,7 +9,47 @@ export class AboutUsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  dellStock() {
+    return setTimeout(() => {
+      return false;
+    });
   }
+
+  hpStock() {
+    return setTimeout(() => {
+      return true
+    });
+  }
+
+  ngOnInit(): void {
+
+    let buyLaptop = new Promise((resolve, reject) => {
+      if (this.dellStock()) {
+        return setTimeout(() => {
+          resolve('dell is buy')
+        }, 3000);
+      } else if (this.hpStock()) {
+        return setTimeout(() => {
+          resolve('hp buy')
+        }, 3000)
+      } else {
+        return setTimeout(() => {
+          reject('please come home')
+        }, 3000);
+
+      }
+      // resolve('Promise  is resolved')
+      // reject('Promise is reject')
+    });
+
+    buyLaptop.then(res => {
+      alert(res);
+    }).catch(res => {
+      alert(res);
+      console.log('res');
+
+    })
+  }
+
 
 }
